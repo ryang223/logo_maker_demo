447f6f25c6e75952f4dedbf9b41552d238fe85b1
ryang223 <ryangoodwd@gmail.com>
2022-10-22 06:50:58 -0400

    Form event works properly and inputs update in sync

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('#logoform');
+const brandInput = document.querySelector('input[name="brandname"]');
+const colorInput = document.querySelector('input[name="color"]');
+const fontSizeInput = document.querySelector('input[name="fontsize"]');
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('submit')
+    console.log(brandInput.value, colorInput.value, fontSizeInput.value)
 })
