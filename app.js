0f027f66f27a6317264b06a4a66df9f042df134a
ryang223 <ryangoodwd@gmail.com>
2022-10-22 07:25:30 -0400

    Works as inteded. next font-size is added

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const form = document.querySelector('#logoform');
 const brandInput = document.querySelector('input[name="brandname"]');
 const colorInput = document.querySelector('input[name="color"]');
 const fontSizeInput = document.querySelector('input[name="fontsize"]');
+const results = document.querySelector('#results');
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('submit')
-    console.log(brandInput.value, colorInput.value, fontSizeInput.value)
-})
+    const newLogo = makeLogo(
+        brandInput.value,
+        colorInput.value, 
+        fontSizeInput.value
+        );
+        results.appendChild(newLogo);
+});
+
+function makeLogo(text, color, size){
+    const logo = document.createElement('h2');
+    logo.innerText = text;
+    logo.style.color = color;
+    logo.style.fontSize = size;
+    return logo;
+}
